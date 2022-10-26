56683c3707068bd694444bb954543815130a2cad
FuzzyKittyz <topher.a@hotmail.com>
2022-10-26 16:57:52 -0600

    changes

--- vampire.js
+++ vampire.js
@@ -10,22 +10,48 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVampire = 0;
+    let currentVampire = this; 
 
+    while (currentVampire.creator) {
+      currentVampire =  currentVampire.creator;
+      numOfVampire++;;
+    }
+    return numOfVampire
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let numOfVampire1 = 0;
+    let numOfVampire2 = 0;
+    let thisVampire = this; 
+    let currentVampire = vampire
 
+    while (currentVampire.creator) {
+      currentVampire =  currentVampire.creator;
+      numOfVampire1++;;
+    }
+    
+    while (thisVampire.creator) {
+      thisVampire = thisVampire.creator;
+      numOfVampire2++
+    }
+    if (numOfVampire2 < numOfVampire1) {
+      return true
+    } else {
+      return false
+    }
   }
 
   /** Stretch **/
